8bbc96f7ad19105f6ce3b3971ba42c5000cf668a
ito-kazuki <ito-kazuki>
2025-08-28 20:28:59 +0900

    ボタンを追加

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 // WebView（ネイティブ）へメッセージ送信
-document.getElementById('sendBtn').onclick = function() {
+document.getElementById('sendBtn1').onclick = function() {
   const message = { type: 'greeting', text: 'こんにちは、WebView!' };
-  // Androidの場合
-  if (window.AndroidBridge && window.AndroidBridge.postMessage) {
-    window.AndroidBridge.postMessage(JSON.stringify(message));
-  }
-  // iOSの場合
-  else if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.bridge) {
-    window.webkit.messageHandlers.bridge.postMessage(message);
-  }
-  // デバッグ用
-  else {
-    alert('WebViewブリッジが見つかりません');
-  }
+  sendParamToNativeApp(message);
+};
+
+// WebView（ネイティブ）へメッセージ送信
+document.getElementById('sendBtn2').onclick = function() {
+  const message = { type: 'greeting', text: 'こんにちは、WebView!' };
+  sendParamToNativeApp(message);
 };
 
 // WebView（ネイティブ）から呼ばれる関数
@@ -20,3 +15,29 @@ window.onNativeMessage = function(msg) {
   let log = document.getElementById('log');
   log.textContent = 'WebViewからのメッセージ: ' + msg;
 };
+
+
+function sendParamToNativeApp(param){
+  // Androidの場合
+  if (window.AndroidBridge && window.AndroidBridge.postMessage) {
+    window.AndroidBridge.postMessage(JSON.stringify(param));
+  }
+  // iOSの場合
+  else if (window.webkit && window.webkit.messageHandlers && window.webkit.messageHandlers.bridge) {
+    window.webkit.messageHandlers.bridge.postMessage(param);
+  }
+  // デバッグ用
+  else {
+    alert('WebViewブリッジが見つかりません');
+  }
+}
+
+function changeLogWithMsg(msg) {
+  let log = document.getElementById('log');
+  log.textContent = msg;
+}
+
+function changeLog() {
+  let log = document.getElementById('log');
+  log.textContent = "Logを変更しました";
+}
